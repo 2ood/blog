ed1e169295eb27388052f9faff51e090ab9c7e5c
2ood <kyungmin.official0@gmail.com>
2022-01-22 01:01:40 +0900

    add : added favicon and loading page

--- html/script/landing_page.js
+++ html/script/landing_page.js
@@ -1,5 +1,15 @@
-const search = document.querySelector(".search");
 const li = document.querySelectorAll("ul li a");
-
-search.classList.add("underline");
 for(i=0;i<li.length;i++) li[i].classList.add("underline");
+
+const load = document.querySelector("#loading");
+const load_cl = load.classList;
+load_cl.add("active");
+load.addEventListener('transitionend', () => {
+  if(load_cl.contains("active")) {
+    load_cl.add("done");
+    load_cl.remove("active");
+  }
+  else if(load_cl.contains("done")) {
+    load.style.zIndex=-1000;
+  }
+});
