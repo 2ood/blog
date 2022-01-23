2a891dcba39a8aee1856a3fac287ea42867ceafc
2ood <kyungmin.official0@gmail.com>
2022-01-23 14:59:41 +0900

    edit : make logo transparent when scrolled

--- html/script/landing_page.js
+++ html/script/landing_page.js
@@ -1,6 +1,15 @@
+let scrollY = window.scrollY;
+window.addEventListener('scroll', () => {
+  scrollY = window.scrollY;
+    logo = document.querySelector(".logo");
+    logo.style.opacity=(scrollY<100?1:(1-scrollY/200))*1.0;
+});
+
+
 const li = document.querySelectorAll("ul li a");
 for(i=0;i<li.length;i++) li[i].classList.add("underline");
 
+/*
 const load = document.querySelector("#loading");
 const load_cl = load.classList;
 load_cl.add("active");
@@ -13,3 +22,4 @@ load.addEventListener('transitionend', () => {
     load.style.zIndex=-1000;
   }
 });
+*/
