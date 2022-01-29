be3944a6695b553fdaddaccc27f6331d1b421560
2ood <kyungmin.official0@gmail.com>
2022-01-29 16:38:53 +0900

    refactor: color made to css var()

---
+++ html/script/footer.js
@@ -0,0 +1,5 @@
+const footer = document.getElementsByTagName("footer")[0];
+footer.innerHTML+=`
+<div> developed by <a href="https://github.com/2ood">2ood.</a></div>
+<div> MIT licensed. 2022</div>
+`;
---
+++ html/script/main.js
@@ -0,0 +1,6 @@
+const to_top = document.querySelector("#to_top");
+if(!(to_top===null)){
+  to_top.addEventListener('click',()=>{
+    console.log("clicked!");
+  });
+}
