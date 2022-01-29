d52b6ee2847cc72a02cfee4a76cd82d0d9e7ffcd
2ood <kyungmin.official0@gmail.com>
2022-01-29 18:41:53 +0900

    refactor : redesigned File system

--- html/script/main.js
+++ html/script/main.js
@@ -1,5 +1,8 @@
 const to_top = document.querySelector("#to_top");
 if(!(to_top===null)){
+  to_top.innerHTML=`<span class="material-icons">
+expand_less
+</span>`;
   to_top.addEventListener('click',()=>{
     console.log("clicked!");
   });
