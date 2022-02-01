e28209061b4b96333f2a2d8a142fde8910678311
2ood <kyungmin.official0@gmail.com>
2022-02-01 22:55:22 +0900

    test of auth

--- html/script/landing_page.js
+++ html/script/landing_page.js
@@ -1,14 +1,2 @@
 const user = auth.currentUser;
 console.log(user);
-
-app.auth().onAuthStateChanged((user) => {
-  if (user) {
-    document.getElementById("login").style.display="flex";
-    var uid = user.uid;
-    console.log("logined");
-
-  } else {
-    document.getElementById("logout").style.display="flex";
-        console.log("logout");
-  }
-});
